e05e69c64a0097bc1ba27b0aaf64c64818e3a287
Galen Guyer <galen@galenguyer.com>
2021-01-31 20:38:48 -0500

    Transaction fee

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -31,8 +31,7 @@ const size_t   CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE     = 10000; //size of 
 const size_t   CRYPTONOTE_COINBASE_BLOB_RESERVED_SIZE        = 600;
 //TODO Define number of digits
 const size_t   CRYPTONOTE_DISPLAY_DECIMAL_POINT              = 8;
-//TODO Define minimum fee for transactions
-const uint64_t MINIMUM_FEE                                   = ;
+const uint64_t MINIMUM_FEE                                   = 10000;
 const uint64_t DEFAULT_DUST_THRESHOLD                        = MINIMUM_FEE;
 
 //TODO Define preferred block's target time
